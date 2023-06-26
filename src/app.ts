d1afcd03cfeba59b781d8c880f486fbcc770f8b3
Eduarda <dudarenno@yahoo.com.br>
2023-06-26 17:04:29 -0300

    Feat: pr.yml

--- src/app.ts
+++ src/app.ts
@@ -11,4 +11,5 @@ server.use(express.json());
 server.use(authRouter);
 server.use(registersRouter)
 
+
 export default server
--- test/integration/authentication.test.ts
+++ test/integration/authentication.test.ts
@@ -1,16 +1,23 @@
 import supertest from 'supertest';
 import server from '../../src/app';
+import prisma from '../../src/config/database';
 
 const api = supertest(server)
+
+// beforeEach(async()=>{
+// await prisma.users.deleteMany() 
+// await prisma.daily_records.deleteMany() 
+// })
+
 describe(`POST /auth/sign-in`, () => {
     describe('when credentials are valid', () => {
-        // it('should respond with status 200', async () => {
-        //   const body = generateValidBody();
-        //   await createUser(body);
-  
-        //   const response = await server.post('/auth/sign-in').send(body);
-  
-        //   expect(response.status).toBe(httpStatus.OK);
-        // });
-})
+        it('should respond with status 200', async () => {
+            //   const body = generateValidBody();
+            //   await createUser(body);
+
+            //   const response = await server.post('/auth/sign-in').send(body);
+
+            //   expect(response.status).toBe(httpStatus.OK);
+        });
+    })
 })
